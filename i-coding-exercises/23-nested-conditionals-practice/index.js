362ee306597ee16e23c1f0960a8dd70943536bd5
Miodrag Simonovic <miodragsimke@gmail.com>
2020-12-08 19:05:22 +0100

    Nested Conditionals Practice

--- i-coding-exercises/23-nested-conditionals-practice/index.js
+++ i-coding-exercises/23-nested-conditionals-practice/index.js
@@ -2,13 +2,13 @@
 const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :) 
 
 // DO NOT TOUCH ANYTHING BELOW (please) 
-if(num <= 100) {
-    if(num >= 50) {
+if (num <= 100) {
+    if (num >= 50) {
         console.log("HEY!");
     }
 } else {
     if (num < 103) {
-        if(num % 2 === 0){
+        if (num % 2 === 0) {
             console.log("YOU GOT ME!");
         }
     }
