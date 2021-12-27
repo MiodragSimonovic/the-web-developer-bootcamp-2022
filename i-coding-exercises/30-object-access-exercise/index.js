2ffaeb759d45e2124fae656169b920778b1e89a7
MiodragSimonovic <miodragsimke@gmail.com>
2021-12-27 23:55:45 +0100

    Object Access Exercise

---
+++ i-coding-exercises/30-object-access-exercise/index.js
@@ -0,0 +1,10 @@
+const restaurant = {
+    name: "Ichiran Ramen",
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: "Brooklyn",
+    state: "NY",
+    zipcode: "11206",
+};
+
+//YOUR CODE GOES DOWN HERE:
+let fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
