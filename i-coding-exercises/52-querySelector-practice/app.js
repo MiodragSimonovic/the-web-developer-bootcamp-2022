accf36c87ede8138e8dd46b77e2cccbfc7872e62
MiodragSimonovic <miodragsimke@gmail.com>
2022-01-18 22:14:03 +0100

    querySelector Practice (edit)

--- i-coding-exercises/52-querySelector-practice/app.js
+++ i-coding-exercises/52-querySelector-practice/app.js
@@ -1,2 +1,2 @@
 const doneTodos = document.querySelectorAll(".done");
-let checkbox = document.querySelector("input[type='checkbox']"); // or just: let checkbox = document.querySelector("#scales");
+const checkbox = document.querySelector("input[type='checkbox']"); // or just: let checkbox = document.querySelector("#scales");
