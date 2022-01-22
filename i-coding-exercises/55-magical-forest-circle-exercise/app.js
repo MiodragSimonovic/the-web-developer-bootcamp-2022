f2fbfe685bed58453b4b84541d77b303f67f54ad
MiodragSimonovic <miodragsimke@gmail.com>
2022-01-22 21:53:54 +0100

    Magical Forest Circle Exercise

---
+++ i-coding-exercises/55-magical-forest-circle-exercise/app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector("#container");
+const image = document.querySelector("#container img");
+
+container.style.textAlign = "center";
+image.style.width = "150px";
+image.style.borderRadius = "50%";
