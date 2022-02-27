0132261ca1c3574edbd8bf2f002c178237ed5465
MiodragSimonovic <miodragsimke@gmail.com>
2022-02-27 22:03:18 +0100

    Input Event Practice (edit1)

--- i-coding-exercises/62-input-event-practice/app.js
+++ i-coding-exercises/62-input-event-practice/app.js
@@ -2,7 +2,7 @@ const h1 = document.querySelector("h1");
 const usernameTextInput = document.querySelector("#username");
 
 function elementUpdateAsTypinginOtherElement(inputElement, outputElement) {
-    if (inputElement.value == "") {
+    if (inputElement.value === "") {
         outputElement.innerText = "Enter Your Username";
     } else {
         outputElement.innerText = `Welcome, ${inputElement.value}`;
